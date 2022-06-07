b900971d17c27a2de7f1d1472428bf19395f4bf9
Mohammed Saqlain <85921230+Mohammedsaklain@users.noreply.github.com>
2022-06-07 19:51:54 +0530

    Update copy.c

--- copy.c
+++ copy.c
@@ -3,22 +3,25 @@
 int main()
 {
 int a[3],b[3],i;
-  printf("enter the first array");
+  printf("enter the array");
   {
        for(i=0;i<3;i++)
        {
         scanf("%d",&a[i]);
        }
+       printf("The first array is\n");
        for(i=0;i<3;i++)
        {
         printf("%d\t",a[i]);
        }
   }
+  printf("\n");
    {
         for(i=0;i<3;i++)
         {
           b[i]=a[i];
         }
+         printf("The copied array elements\n");
         for(i=0;i<3;i++)
         {
          printf("%d\t",b[i]);
